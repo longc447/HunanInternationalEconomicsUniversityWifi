b19eaae1b5d814e4a14515fe7166aaaa08911881
344403155@qq.com <344403155@qq.com>
2021-04-01 01:30:30 +0800

    测试网络从请求百度页面改为检查dns

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const phone = require('./lib/phone')
 const log = require('./lib/log')
 const net = require('./lib/net')
 
-// let p = phone.get()
-// log.set(p)
-// net.connectWifi(p)
 log.set("正在监听网络连接。。。测试：http://www.baidu.com")
 setInterval(() => {
     net.hasNet()
