1317bc19b582dbc5cf2412b16b0b84eab979bf7a
344403155@qq.com <344403155@qq.com>
2021-04-04 19:35:31 +0800

    新增程序因测试网页编码异常终止的处理

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const phone = require('./lib/phone')
 const log = require('./lib/log')
 const net = require('./lib/net')
 
-log.set("正在监听网络连接。。。测试：http://www.baidu.com")
-setInterval(() => {
-    net.hasNet().catch(res=>{
-        let p = phone.get()
-        net.connectWifi(p)
-        log.set(`断线重连，正在尝试账号：${p}`)
-    })
-}, 2000);
+function listenNet() {
+    try {
+        log.set("正在监听网络连接。。。测试：http://www.baidu.com")
+        setInterval(() => {
+            net.hasNet().catch(res => {
+                let p = phone.get()
+                net.connectWifi(p)
+                log.set(`断线重连，正在尝试账号：${p}`)
+            })
+        }, 2000);
+    } catch (error) {
+        var spawn = require('child_process').spawn;
+        log.set('程序异常终止')
+        free = spawn('clear');
+        listenNet();
+    }
+}
+listenNet();
+
+
+
--- test.js
+++ test.js
@@ -1,4 +1,41 @@
-const log=require('./lib/log')
-const tool=require('./lib/tool')
-tool.dateFormat()
-log.set('logtext')
+const log = require('./lib/log')
+const tool = require('./lib/tool')
+
+
+
+
+var spawn = require('child_process').spawn;
+
+
+
+try {
+    tool.dateFormat()
+    log.set('logtext')
+} catch {
+
+    free = spawn('clear');
+    // 捕获标准输出并将其打印到控制台 
+
+    // free.stdout.on('data', function (data) { 
+
+    //     console.log("clear \n"+data)
+    //     console.log('standard output:\n' + data); 
+
+    // });
+    // // 捕获标准错误输出并将其打印到控制台 
+
+    // free.stderr.on('data', function (data) { 
+
+    // console.log('standard error output:\n' + data); 
+
+    // });
+    // 注册子进程关闭事件 
+
+//     free.on('exit', function (code, signal) { 
+
+//     console.log('child process eixt ,exit:' + code); 
+
+// });
+
+
+}
