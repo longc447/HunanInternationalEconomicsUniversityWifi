4ee686f1d907f6325f5cc7f24d8ce5d37ba20af4
344403155@qq.com <344403155@qq.com>
2021-04-04 19:36:15 +0800

    不提交test

--- test.js
+++ test.js
@@ -14,28 +14,4 @@ try {
 } catch {
 
     free = spawn('clear');
-    // 捕获标准输出并将其打印到控制台 
-
-    // free.stdout.on('data', function (data) { 
-
-    //     console.log("clear \n"+data)
-    //     console.log('standard output:\n' + data); 
-
-    // });
-    // // 捕获标准错误输出并将其打印到控制台 
-
-    // free.stderr.on('data', function (data) { 
-
-    // console.log('standard error output:\n' + data); 
-
-    // });
-    // 注册子进程关闭事件 
-
-//     free.on('exit', function (code, signal) { 
-
-//     console.log('child process eixt ,exit:' + code); 
-
-// });
-
-
 }
