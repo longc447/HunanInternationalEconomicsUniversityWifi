e8e7e903fc2b048214bbdd35214ceb3b41a213a9
344403155@qq.com <344403155@qq.com>
2021-04-08 01:46:17 +0800

    新增使用长时连接功能，使连接更稳定

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const phone = require('./lib/phone')
 const log = require('./lib/log')
 const net = require('./lib/net')
-
+let start = null, end = null, nowphone = null,usetime=null;
+let useLongtimePhone =false;//默认不使用长时连接账号，因为首次使用没有长时间连接账号
 function listenNet() {
     try {
         log.set("正在监听网络连接。。。测试：http://www.baidu.com")
         setInterval(() => {
-            net.hasNet().catch(res => {
-                let p = phone.get()
-                net.connectWifi(p)
-                log.set(`断线重连，正在尝试账号：${p}`)
-            })
+            net.hasNet()
+                .then(res => {
+                    if (!start) {
+                        start = Date.parse(new Date()) 
+                    }
+                })
+                .catch(res => {
+                    //结算时间
+                    if(start){
+                        end = Date.parse(new Date()) 
+                        usetime=end-start
+                        start=null
+                        end=null
+                        //写入计时账号/如果是上次的则不写入
+                        if(nowphone)phone.saveLongPhone(JSON.parse(`{"${nowphone}":"${usetime}"}`))
+                    }
+                    //重连
+                    useLongtimePhone=!useLongtimePhone;//间隔一个手机号使用长连接
+                    nowphone = useLongtimePhone?phone.get():phone.getMaxTimePhone();
+                    net.connectWifi(nowphone)
+                    log.set(`断线重连，正在尝试账号：${nowphone}`)
+                })
         }, 2000);
     } catch (error) {
         var spawn = require('child_process').spawn;
